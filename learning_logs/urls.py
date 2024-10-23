8bdab5a772cc073ac2c8cb48b540ce4fa069cc4e
Matheus Veiga <cavalcante.matheusveiga@gmail.com>
2024-10-22 22:47:32 -0300

    wip

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('topic/<topic_id>/', views.topic, name='topic'),
     path('new-topic/', views.new_topic, name='new-topic'),
     path('new-entry/<topic_id>', views.new_entry, name='new-entry'),
+    path('edit-entry/<entry_id>', views.edit_entry, name='edit-entry'),
 ]
